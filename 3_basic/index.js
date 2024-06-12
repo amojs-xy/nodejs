2ae82850a6172365a0ca036dc8efa490632c7aa3
xiaoye <amo.xiaoye@gmail.com>
2024-06-12 15:28:10 +0800

    add some

--- 3_basic/index.js
+++ 3_basic/index.js
@@ -44,3 +44,41 @@
 // console.log(data.toString());
 
 // console.log('test');
+
+// ---------------------------------------
+const {
+    readFileSync,
+    writeFile
+} = require('fs');
+const {
+    resolve
+} = require('path');
+
+const data = [
+    {
+        name: 'iPhone 15',
+        field: 'iphone'
+    },
+    {
+        name: 'Huawei Pura 70',
+        field: 'huawei'
+    },
+    {
+        name: 'Xiaomi 14',
+        field: 'xiaomi'
+    }
+]
+
+data.forEach(item => {
+    const path = resolve(__dirname, `data/${ item.field }.json`);
+    const data = JSON.parse(readFileSync(path, 'utf8'));
+    data.push(item.name);
+    writeFile(path, JSON.stringify(data), (err) => {
+        console.log(item.name + '写入完成');
+        if (err) {
+            throw new Error('写入文件错误');
+        }
+    });
+
+    console.log(item.name + '准备写入...');
+});
